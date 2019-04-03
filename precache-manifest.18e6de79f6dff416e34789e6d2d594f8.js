7c74f7e1a0b1131d39c0c3bf815fee7916a8a7d5
Yasir S <yasser9946.1@gmail.com>
2019-04-03 16:52:04 +0300

    Updates

---
+++ precache-manifest.18e6de79f6dff416e34789e6d2d594f8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fef87711f34baf3f448e25a3519e290",
+    "url": "/pr2/static/media/maths.0fef8771.png"
+  },
+  {
+    "revision": "3f7777666bd6c71b69be",
+    "url": "/pr2/static/css/main.5122b4ae.chunk.css"
+  },
+  {
+    "revision": "7ecf2f5bcabf5bec6637",
+    "url": "/pr2/static/js/runtime~main.d0ea7803.js"
+  },
+  {
+    "revision": "19f449b6ce878f023d64",
+    "url": "/pr2/static/js/2.d6bfa60f.chunk.js"
+  },
+  {
+    "revision": "5f95db0d6910113d999392737123cceb",
+    "url": "/pr2/static/media/map.5f95db0d.png"
+  },
+  {
+    "revision": "80d248505a08747bf0565137e6e26c75",
+    "url": "/pr2/static/media/scroll.80d24850.png"
+  },
+  {
+    "revision": "3f7777666bd6c71b69be",
+    "url": "/pr2/static/js/main.18110198.chunk.js"
+  },
+  {
+    "revision": "2d686acd2384ec55ddd9cd6fbdb28e2f",
+    "url": "/pr2/static/media/bike.2d686acd.png"
+  },
+  {
+    "revision": "57aa59fc812103ab30e2e7fc8538294b",
+    "url": "/pr2/static/media/movie.57aa59fc.png"
+  },
+  {
+    "revision": "65de928e9664ef654d1d91c0feaa23eb",
+    "url": "/pr2/static/media/intellectual.65de928e.png"
+  },
+  {
+    "revision": "ce78a029b797ed8abf75b273d7b9f763",
+    "url": "/pr2/static/media/pets.ce78a029.png"
+  },
+  {
+    "revision": "f87fa98a44e09542f32d64729ea142e4",
+    "url": "/pr2/index.html"
+  }
+];
